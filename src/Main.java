3ee913e9b84ca18264b1a3470f540b9cca9a59ff
e146508 <Dileep.Kessireddy@wnco.com>
2023-11-16 09:18:44 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+//       Item coke = new Item("drink", "coke", 1.50);
+//       coke.printItem();;
+//       coke.setSize("LARGE");
+//       coke.printItem();
+//
+//       Item avocado = new Item("Toppings", "avocado", 1.50);
+//       avocado.printItem();
+
+//        Burger burger = new Burger("regular", 4.00);
+//        burger.addToppings("BACON", "CHEESE", "MAYO");
+//        burger.printItem();
+
+//        MealOrder reqularMeal = new MealOrder();
+//        reqularMeal.addBurgerToppings("BACON", "CHEESE", "MAYO");
+//        reqularMeal.setDrinkSize("LARGE");
+//        reqularMeal.printItemizedList();
+
+//        MealOrder secondMeal = new MealOrder("turkey",
+//                "7-up", "chili");
+//        secondMeal.addBurgerToppings("LETTUCE", "CHEESE", "MAYO");
+//        secondMeal.setDrinkSize("SMALL");
+//        secondMeal.printItemizedList();
+
+        MealOrder deluxeMeal = new MealOrder("deluxe", "7-up", "chili");
+        deluxeMeal.addBurgerToppings("AVOCADO", "BACON", "LETTUCE", "CHEESE", "MAYO");
+        deluxeMeal.setDrinkSize("SMALL");
+        deluxeMeal.printItemizedList();
+
+
+    }
+}
